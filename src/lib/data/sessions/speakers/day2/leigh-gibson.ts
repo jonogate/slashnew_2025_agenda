c53e8c8cc94018d03313c47ef7b4130106df281f
jonogate <44513508+jonogate@users.noreply.github.com>
2025-04-30 16:32:30 +1000

    Add Samantha Davies to leigh-gibson.ts
    Just updating the copy, not changing the slug

--- src/lib/data/sessions/speakers/day2/leigh-gibson.ts
+++ src/lib/data/sessions/speakers/day2/leigh-gibson.ts
@@ -16,6 +16,18 @@ Starting out as an engineer, Leigh quickly worked out that solution designers ha
 `,
 			image: 'leigh-gibson.jpg',
 			jobTitle: 'Group Owner AI Enabled Employees at Telstra'
+		},
+		{
+			name: 'Samantha Davies',
+			bio: `
+Samantha Davies is the Cloud and Infrastructure Centre of Expertise (CCOE) Lead at Telstra.  Sam leads multiple teams across Cloud Security, Service Management, Automation and Cloud Economics and is a key strategy lead for the cloud and infrastructure capability in Telstra. 
+
+Sam joined Telstra almost 3 years ago and prior to that she was with ANZ Bank for 11 years where she was the key lead in their Cloud Simplification and Transformation program.  Prior to that she was the ANZ Cyber Defence Lead where she managed teams across multiple regions protecting ANZ bank on a day to day basis.  
+
+Samantha has lived and worked in seven (7) countries during her career in technology and started out as a software developer, database administrator and development lead.  Sam is known for her passion and positivity and is often referred to as the "glue" who brings teams together.  Her passion for diversity in technology has led to her starting a not-for-profit called Tech Exec Women (TEW) with her co-director Leigh Gibson which supports, connects and empowers women leaders in technology.
+      `,
+			image: 'sam-davies.jpg',
+			jobTitle: 'Cloud and Infrastructure Centre of Expertise (CCOE) Lead at Telstra'
 		}
 	]
 };
